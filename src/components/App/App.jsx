590735e7b9b21e7605b34ce8df3a6876409d68ba
Vitalii <149200949+Velykobrat@users.noreply.github.com>
2024-05-14 13:17:47 +0300

    Завдання 3 - Історія транзакцій
    1. Створив компонент
    2. підключив до основного компонента та підключив .json

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,8 +2,10 @@
 
 import Profile from '../Profile/Profile';
 import FriendList from '../FriendList/FriendList';
+import TransactionHistory from "../TransactionHistory/TransactionHistory";
 import userData from "../../userData.json";
 import friends from "../../friends.json";
+import transactions from "../../transactions.json";
 
 const App = () => {
   return (
@@ -17,6 +19,9 @@ const App = () => {
       />
       <FriendList
         friends={friends}
+      />
+      <TransactionHistory
+        items={transactions}
       />
     </>
   );
